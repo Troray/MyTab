491bc98113b211442c3fbbd93743e184287c6f7a
Troray <troraycom@gmail.com>
2026-09-02 09:32:17 +0800

    style(ui): remove animation delays in ToggleSwitch

--- src/mytab/components/ToggleSwitch.tsx
+++ src/mytab/components/ToggleSwitch.tsx
@@ -24,9 +24,9 @@ export const ToggleSwitch: React.FC<ToggleSwitchProps> = ({
         aria-checked={checked}
       />
       <div
-        className={`w-9 h-5 rounded-full transition-colors peer peer-focus-visible:ring-2 peer-focus-visible:ring-offset-2 ${
+        className={`w-9 h-5 rounded-full duration-0 peer peer-focus-visible:ring-2 peer-focus-visible:ring-offset-2 ${
           isLight ? 'peer-focus-visible:ring-slate-900 peer-focus-visible:ring-offset-white' : 'peer-focus-visible:ring-white peer-focus-visible:ring-offset-slate-900'
-        } peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all ${
+        } peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:duration-0 ${
           isLight
             ? 'bg-slate-300 peer-checked:bg-slate-900'
             : 'bg-white/20 peer-checked:bg-white after:peer-checked:bg-slate-900'
