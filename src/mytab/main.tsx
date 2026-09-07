1d76a81c2d9cf0f5c2b6cd16f31e402e4fc67360
Troray <troraycom@gmail.com>
2026-09-07 14:38:02 +0800

    feat(ui): wrap root application with ErrorBoundary component

--- src/mytab/main.tsx
+++ src/mytab/main.tsx
@@ -4,11 +4,14 @@ import { App } from './App';
 import './styles/index.css';
 
 import { loadAppState } from '../services/storage';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 loadAppState().then((initialState) => {
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-      <App initialState={initialState} />
+      <ErrorBoundary>
+        <App initialState={initialState} />
+      </ErrorBoundary>
     </React.StrictMode>
   );
 });
